b9990cb12a468fb4b8084a9f2c0621ab7b008b2a
manon <manon.gardin@gmail.com>
2024-01-23 19:06:46 +0100

    Add functions delete, MarkAsComplete

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ $ 5 Display done items`;
 
 function addTaskCommand(newTask) {
     fs.appendFileSync('todo.txt', `\n${newTask}`);
+    showTodoList();
 }
 
 function showTodoList() {
@@ -65,6 +66,53 @@ function MarkAsComplete(index){
     }
 }
 
+function deleteTask(index) {
+    try {
+        const data = fs.readFileSync('todo.txt', 'utf-8');
+        const tasks = data.split('\n');
+
+        if (index <= 0 || index > tasks.length){
+            console.log('The index is not correct.');
+        }
+        else{
+            const item = tasks[index];
+
+            const removedTask = tasks.splice(index, 1)[0];
+            const updatedTasks = tasks.join('\n');
+            fs.writeFileSync('todo.txt', updatedTasks);
+        }
+
+        if (tasks.length > 1) {
+            console.log(tasks[0]);
+            for (let index = 1; index < tasks.length; index++) {
+                console.log(`${index}. ${tasks[index]}`);
+            }
+        } else {
+            console.log('The to-do list is empty.');
+        }
+    } catch (error) {
+        console.error('An error occurred while reading the todo.txt file:', error.message);
+    }
+}
+
+function showDoneList(){
+    try {
+        const data = fs.readFileSync('done.txt', 'utf-8');
+        const tasks = data.split('\n');
+
+        if (tasks.length > 1) { // Check if there is more than one line (title + tasks)
+            console.log(tasks[0]); // Display the title without an index
+            for (let index = 1; index < tasks.length; index++) {
+                console.log(`${index}. ${tasks[index]}`);
+            }
+        } else {
+            console.log('The done list is empty.');
+        }
+    } catch (error) {
+        console.error('An error occurred while reading the done.txt file:', error.message);
+    }
+}
+
 function main() {
     MenuFunction();
 
@@ -75,13 +123,23 @@ function main() {
             showTodoList();
             break;
         case '2':
+            showTodoList();
             const newTask = readline.question('Enter the new task: ');
             addTaskCommand(newTask);
+            break;
+        case '3':
+            showTodoList();
+            const delTask = readline.question('Enter the task you want to delete: ');
+            deleteTask(delTask);
             break;
         case '4':
             showTodoList();
             const index_del = readline.question('Choose an item to mark as complete: ');
             MarkAsComplete(index_del);
+            showDoneList();
+            break;
+        case '5':
+            showDoneList();
             break;
     }
 }
